cf12372a5900526c7fc5e770831b0cc5fce7c287
bshawn <ershov.connect@gmail.com>
2022-01-02 01:11:30 +0300

    Почти

--- parse/dir_parse.c
+++ parse/dir_parse.c
@@ -6,11 +6,51 @@
 /*   By: bshawn <bshawn@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/02 00:56:03 by bshawn            #+#    #+#             */
-/*   Updated: 2022/01/02 00:59:42 by bshawn           ###   ########.fr       */
+/*   Updated: 2022/01/02 01:10:39 by bshawn           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
+
+static int	dir(t_cmd *cmd, t_list *tmp)
+{
+	cmd->f_direct = DIR;
+	if (tmp->next)
+		cmd->name_file = ft_strdup(tmp->next->val);
+	if (!cmd->name_file)
+		return (error_return_int());
+	return (0);
+}
+
+static int	doub_dir(t_cmd *cmd, t_list *tmp)
+{
+	cmd->f_direct = DOUB_DIR;
+	if (tmp->next)
+		cmd->name_file = ft_strdup(tmp->next->val);
+	if (!cmd->name_file)
+		return (error_return_int());
+	return (0);
+}
+
+static int redir(t_cmd *cmd, t_list *tmp)
+{
+	cmd->f_direct = REDIR;
+	if (tmp->next)
+		cmd->name_file = ft_strdup(tmp->next->val);
+	if (!cmd->name_file)
+		return (error_return_int());
+	return (0);
+}
+
+static int doub_redir(t_cmd *cmd, t_list *tmp)
+{
+	cmd->f_direct = DOUB_REDIR;
+	if (tmp->next)
+		cmd->name_file = ft_strdup(tmp->next->val);
+	if (!cmd->name_file)
+		return (error_return_int());
+	return (0);
+}
 
 int	dir_parse(t_cmd *cmd)
 {
@@ -19,41 +59,13 @@ int	dir_parse(t_cmd *cmd)
 	while (tmp)
 	{
 		if (!ft_strcmp(tmp->val, ">"))
-		{
-			cmd->f_direct = DIR;
-			if (tmp->next)
-				cmd->name_file = ft_strdup(tmp->next->val);
-			if (!cmd->name_file)
-				return (error_return_int());
-			return (0);
-		}
+			return (dir(cmd, tmp));
 		else if (!ft_strcmp(tmp->val, ">>"))
-		{
-			cmd->f_direct = DOUB_DIR;
-			if (tmp->next)
-				cmd->name_file = ft_strdup(tmp->next->val);
-			if (!cmd->name_file)
-				return (error_return_int());
-			return (0);
-		}
+			return (doub_dir(cmd, tmp));
 		else if (!ft_strcmp(tmp->val, "<"))
-		{
-			cmd->f_direct = REDIR;
-			if (tmp->next)
-				cmd->name_file = ft_strdup(tmp->next->val);
-			if (!cmd->name_file)
-				return (error_return_int());
-			return (0);
-		}
+			return (redir(cmd, tmp));
 		else if (!ft_strcmp(tmp->val, "<<"))
-		{
-			cmd->f_direct = DOUB_REDIR;
-			if (tmp->next)
-				cmd->name_file = ft_strdup(tmp->next->val);
-			if (!cmd->name_file)
-				return (error_return_int());
-			return (0);
-		}
+			return (doub_redir(cmd, tmp));
 		else
 		{
 			cmd->f_direct = NONE;
